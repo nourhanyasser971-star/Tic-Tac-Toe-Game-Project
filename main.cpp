684d43f0b8238835febac4c922cb85c5e3b69599
janna ahmed <janna.shawky.2006@gmail.com>
2026-09-13 13:35:13 +0300

    Implement AIPlayer class part1

--- main.cpp
+++ main.cpp
@@ -2,40 +2,13 @@
 #include <vector>
 #include <string>
 
+
 using namespace std;
 
 enum Difficulty {EASY, HARD};
 
-class Player{
-protected:
-  string name;
-  char symbol;
-public:
-  Player(string inputName, char inputSymbol)
-  {
-    name = inputName;
-    symbol = inputSymbol;
-  }
 
-  virtual ~Player()
-  {}
 
-  virtual void getMove(int& row, int& col)= 0;
-
-  string getName() const
-  {
-    return name;
-  }
-  char getSymbol() const
-  {
-    return symbol;
-  }
-
-  void setName(string Name)
-  {
-    name = Name;
-  }
-};
 class Board{
 private:
     vector<vector<char>> grid;
@@ -71,6 +44,82 @@ public:
 
 
 };
+
+
+
+class Player{
+protected:
+  string name;
+  char symbol;
+public:
+  Player(string inputName, char inputSymbol)
+  {
+    name = inputName;
+    symbol = inputSymbol;
+  }
+
+  virtual ~Player()
+  {}
+
+  virtual void getMove(int& row, int& col)= 0;
+
+  string getName() const
+  {
+    return name;
+  }
+  char getSymbol() const
+  {
+    return symbol;
+  }
+
+  void setName(string Name)
+  {
+    name = Name;
+  }
+};
+
+
+
+class AIPlayer:public Player{
+  private:
+
+   Difficulty difficulty;
+
+  public:
+   AIPlayer(const string& n, char s, Difficulty d):Player(n,s),difficulty(d){}
+
+   void setDifficulty(Difficulty newDifficulty){
+     difficulty=newDifficulty;
+
+   }
+   void getRandomMove(const Board& board, int& row, int& col)const {
+     vector<pair<int,int>> valid;
+     int size=board.getSize();
+     for(int i=0;i<size;i++){
+      for(int j=0;j<size;j++){
+
+        bool isValid=board.isValidMove(i,j);
+
+        if(isValid){
+          valid.push_back({i,j});
+        }
+
+      }
+     }
+     if(valid.empty()){
+       return;
+     }
+     int r=rand()%valid.size();
+     row=valid[r].first;
+     col=valid[r].second;
+
+   }
+
+
+};
+
+
+
 int main(){
 
  return 0;
